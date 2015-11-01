25c018bb6b22e3c1b2d1b07783bd5ff3bb9cc574
Martin Vestager Ottesen <martin_v_ottesen@hotmail.com>
2015-11-01 17:13:03 +0100

    Changed folder structure

---
+++ Exercises/HelloWorldClient/build/generated/jax-wsCache/HeloWorldServiceService/dk/dtu/ws/ObjectFactory.java
@@ -0,0 +1,71 @@
+
+package dk.dtu.ws;
+
+import javax.xml.bind.JAXBElement;
+import javax.xml.bind.annotation.XmlElementDecl;
+import javax.xml.bind.annotation.XmlRegistry;
+import javax.xml.namespace.QName;
+
+
+/**
+ * This object contains factory methods for each 
+ * Java content interface and Java element interface 
+ * generated in the dk.dtu.ws package. 
+ * <p>An ObjectFactory allows you to programatically 
+ * construct new instances of the Java representation 
+ * for XML content. The Java representation of XML 
+ * content can consist of schema derived interfaces 
+ * and classes representing the binding of schema 
+ * type definitions, element declarations and model 
+ * groups.  Factory methods for each of these are 
+ * provided in this class.
+ * 
+ */
+@XmlRegistry
+public class ObjectFactory {
+
+    private final static QName _HelloWorld_QNAME = new QName("http://ws.dtu.dk/", "helloWorld");
+    private final static QName _HelloWorldResponse_QNAME = new QName("http://ws.dtu.dk/", "helloWorldResponse");
+
+    /**
+     * Create a new ObjectFactory that can be used to create new instances of schema derived classes for package: dk.dtu.ws
+     * 
+     */
+    public ObjectFactory() {
+    }
+
+    /**
+     * Create an instance of {@link HelloWorldResponse }
+     * 
+     */
+    public HelloWorldResponse createHelloWorldResponse() {
+        return new HelloWorldResponse();
+    }
+
+    /**
+     * Create an instance of {@link HelloWorld }
+     * 
+     */
+    public HelloWorld createHelloWorld() {
+        return new HelloWorld();
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link HelloWorld }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws.dtu.dk/", name = "helloWorld")
+    public JAXBElement<HelloWorld> createHelloWorld(HelloWorld value) {
+        return new JAXBElement<HelloWorld>(_HelloWorld_QNAME, HelloWorld.class, null, value);
+    }
+
+    /**
+     * Create an instance of {@link JAXBElement }{@code <}{@link HelloWorldResponse }{@code >}}
+     * 
+     */
+    @XmlElementDecl(namespace = "http://ws.dtu.dk/", name = "helloWorldResponse")
+    public JAXBElement<HelloWorldResponse> createHelloWorldResponse(HelloWorldResponse value) {
+        return new JAXBElement<HelloWorldResponse>(_HelloWorldResponse_QNAME, HelloWorldResponse.class, null, value);
+    }
+
+}
