5be449db4ced1dec4e14aaa8d121cb358780078f
Martin Vestager Ottesen <martin_v_ottesen@hotmail.com>
2015-11-09 13:48:33 +0100

    created the project

---
+++ Project/LameDuck/src/java/dk/dtu/ws/AirlineReservation.java
@@ -0,0 +1,25 @@
+package dk.dtu.ws;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Martin
+ */
+
+@javax.jws.WebService
+public class AirlineReservation {
+    
+    public String getFlights(String Start, String Destination, Date date){
+        return "";
+    }
+    
+    public String bookFlight(){
+        return "";
+    }
+    
+    public String cancelFlight(){
+        return "";
+    }
+    
+}
